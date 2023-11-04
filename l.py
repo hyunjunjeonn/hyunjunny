91aaa0ea84ea248fc6eba779ddd1afe24b3ed785
hyunjunjeonn <149846558+hyunjunjeonn@users.noreply.github.com>
2023-11-04 10:43:15 +0900

    Update l.py

--- l.py
+++ l.py
@@ -1,2 +1,14 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
 st.title('hello')
+
+dataframe = pd.DataFrame({
+  'first column': [3, 2, 3, 4],
+  'second column': [30, 20, 30, 40]
+})
+
+st.dataframe(dataframe, use_container_width=False)
+
+st.table(dataframe)
