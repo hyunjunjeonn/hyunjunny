99a0a154f3fff58e65cf48474ba12947ab601fac
hyunjunjeonn <149846558+hyunjunjeonn@users.noreply.github.com>
2023-11-11 09:33:32 +0900

    Update l.py

--- l.py
+++ l.py
@@ -16,3 +16,8 @@ st.table(dataframe)
 
 st.metric(label="온도", value="10'C", delta="1.2C")
 st.metric(label="삼성전자", value="61000 원", delta="-3200 원")
+
+col1, col2, col3 = st.columes(3)
+col1.metric(label="달러USD", value="1200 원", delta="-200 원")
+col2.metric(label="일본JPY(100엔)", value="958.62 원", delta="-2.44 원")
+col3.metric(label="유럽연합EUR", value="1,335.85 원", delta="1.24 원")
